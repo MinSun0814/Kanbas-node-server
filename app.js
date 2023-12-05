844d59ed05021481e56299441c16815abbdcc54a
Min Sun <sun.min1@northeastern.edu>
2023-12-04 22:51:05 -0500

    d

--- app.js
+++ app.js
@@ -1,39 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import session from 'express-session';
-import mongoose from 'mongoose';
-import "dotenv/config";
 import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
-import UserRoutes from "./users/routes.js";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
-
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
 const app = express();
 app.use(express.json());
-app.use(cors({
-  credentials: true,
-  origin: process.env.FRONTEND_URL
-}));
-
-const sessionOptions = {
-  secret: "any string",
-  resave: false,
-  saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-  };
-}
-app.use(session(sessionOptions));
-
-
-UserRoutes(app);
+app.use(cors());
 HelloRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
